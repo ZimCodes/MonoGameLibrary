8221310880edd60b312d5bb6fff357d90fec568a
ZimCodes <23222943+ZimCodes@users.noreply.github.com>
2020-09-06 03:41:41 -0500

    Adds method to check remaining time for a playing Song; Documented remaining Audio classes

--- Audio/MultiMediaPlayer.cs
+++ Audio/MultiMediaPlayer.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
+using System.Windows.Forms;
 
 namespace MonoGameLibrary.Audio
 {
@@ -13,12 +14,20 @@ namespace MonoGameLibrary.Audio
         private static SongList songList;
         private static int curSongIndex;
         /// <summary>
-        /// Loops the current Song playing
+        /// Enable or Disable Looping of Songs being played
         /// </summary>
         public static bool isLooped { get { return MediaPlayer.IsRepeating; } set { MediaPlayer.IsRepeating = value; } }
+        /// <summary>
+        /// Mutes the volume of current Song
+        /// </summary>
         public static bool isMuted { get { return MediaPlayer.IsMuted; } set { MediaPlayer.IsMuted = value; } }
+        /// <summary>
+        /// Change the volume. 0.0(silence) to 1.0(full) volume
+        /// </summary>
         public static float Volume { get { return MediaPlayer.Volume; } set { MediaPlayer.Volume = value; } }
-
+        /// <summary>
+        /// Instance of the current Song
+        /// </summary>
         public static Song CurrentSong { get { return songList.GetMedia(curSongIndex); } }
         static MultiMediaPlayer()
         {
@@ -26,34 +35,56 @@ namespace MonoGameLibrary.Audio
             
             songList = new SongList();
         }
+        /// <summary>
+        /// Adds Song(s) to the Song list to be played
+        /// </summary>
+        /// <param name="mediaItems">Songs to play</param>
         public static void AddToQueue(params Song[] mediaItems)
         {
             songList.Add(mediaItems);
         }
+        /// <summary>
+        /// Adds Song(s) to MediaPlayer list to be played
+        /// </summary>
+        /// <param name="content">THe content manager to use in order to load the content</param>
+        /// <param name="songs">Name of the songs to load into the Song list</param>
         public static void AddToQueue(ContentManager content,IEnumerable<string> songs)
         {
             songList.Add(content, songs);
         }
+        /// <summary>
+        /// Clear the Song list
+        /// </summary>
         public static void ClearQueue()
         {
             curSongIndex = 0;
             songList.Clear();
         }
-
+        /// <summary>
+        /// Plays the next Song
+        /// </summary>
         public static void Next()
         {
             curSongIndex = curSongIndex + 1 >= songList.Length ? 0 : curSongIndex + 1;
             Play();
         }
-
+        /// <summary>
+        /// Pauses the Song
+        /// </summary>
         public static void Pause()
         {
             MediaPlayer.Pause();
         }
+        /// <summary>
+        /// Resume playing the current Song
+        /// </summary>
         private static void Resume()
         {
             MediaPlayer.Resume();
         }
+        /// <summary>
+        /// Plays the current Song. If Song is paused, Resume it instead of starting over
+        /// </summary>
         public static void Play()
         {
             if (MediaPlayer.State == MediaState.Paused)
@@ -63,34 +94,65 @@ namespace MonoGameLibrary.Audio
             MediaPlayer.Play(songList.GetMedia(curSongIndex));
             
         }
-
+        /// <summary>
+        /// Plays a previous Song 
+        /// </summary>
         public static void Previous()
         {
             curSongIndex = curSongIndex - 1 < 0 ? songList.Length - 1 : curSongIndex - 1;
             Play();
         }
-
+        /// <summary>
+        /// Removes a Song from the Song list
+        /// </summary>
+        /// <param name="mediaName">Name of the Song to remove</param>
         public static void RemoveFromQueue(string mediaName)
         {
             songList.Remove(mediaName);
         }
-
+        /// <summary>
+        /// Removes a Song from the Song list
+        /// </summary>
+        /// <param name="index">THe index of the Song to remove</param>
         public static void RemoveFromQueue(int index)
         {
             songList.RemoveAt(index);
         }
+        /// <summary>
+        /// Checks whether the Song is in the Song list
+        /// </summary>
+        /// <param name="mediaName">Name of the Song</param>
+        /// <returns>True or False when Song is in the list</returns>
         public static bool HasSongInList(string mediaName)
         {
             return songList.Contains(mediaName);
         }
+        /// <summary>
+        /// Stops Song from playing, restarting the Song from the beginning.
+        /// </summary>
         public static void Stop()
         {
             MediaPlayer.Stop();
         }
+        /// <summary>
+        /// Check if the current Song has finished playing
+        /// </summary>
+        /// <returns>True or False if song is finished</returns>
         public static bool IsSongFinished()
         {
             return MediaPlayer.PlayPosition >= CurrentSong.Duration;
         }
+        /// <summary>
+        /// The amount of time remaining in the song
+        /// </summary>
+        /// <returns>The elapsed time remaining of the song</returns>
+        public static TimeSpan ElapsedSongTime()
+        {
+            return CurrentSong.Duration - MediaPlayer.PlayPosition;
+        }
+        /// <summary>
+        /// Shuffles the Song list using cryptographic algorithm
+        /// </summary>
         public static void ShuffleSongs()
         {
             songList.shuffleSongs();
--- Audio/SongList.cs
+++ Audio/SongList.cs
@@ -10,19 +10,35 @@ namespace MonoGameLibrary.Audio
 {
     class SongList : IMediaList<Song>
     {
+        /// <summary>
+        /// List of songs
+        /// </summary>
         private List<Song> songs;
+        /// <summary>
+        /// The amount of Songs in the list
+        /// </summary>
         public int Count { get { return this.songs.Count; } }
-
+        /// <summary>
+        /// The amount of Songs in the list
+        /// </summary>
         public int Length { get { return this.songs.Count; } }
         public SongList()
         {
             this.songs = new List<Song>();
         }
+        /// <summary>
+        /// Adds the Song to the list
+        /// </summary>
+        /// <param name="mediaItems">Songs to add</param>
         public void Add(params Song[] mediaItems)
         {
             this.songs.AddRange(mediaItems);
         }
-
+        /// <summary>
+        /// Adds the Song to the list
+        /// </summary>
+        /// <param name="content">ContentManager reference to load Song</param>
+        /// <param name="mediaNames">Names of the songs to add to the list</param>
         public void Add(ContentManager content, IEnumerable<string> mediaNames)
         {
             foreach (string name in mediaNames)
@@ -31,39 +47,62 @@ namespace MonoGameLibrary.Audio
                 this.songs.Add(s);
             }
         }
-
+        /// <summary>
+        /// Clears the song list
+        /// </summary>
         public void Clear()
         {
             this.songs.Clear();
         }
-
+        /// <summary>
+        /// Check to see if list contains the Song
+        /// </summary>
+        /// <param name="mediaName"></param>
+        /// <returns>True or false if Song is in list</returns>
         public bool Contains(string mediaName)
         {
             Song s = this.songs.Find(x => x.Name == mediaName);
             return this.songs.Contains(s);
         }
-
+        /// <summary>
+        /// Retrieve a specified Song
+        /// </summary>
+        /// <param name="index">Index of the song</param>
+        /// <returns>THe Song reference</returns>
         public Song GetMedia(int index)
         {
             
             return this.songs[index];
         }
-        
+        /// <summary>
+        /// Retrieve a specified Song
+        /// </summary>
+        /// <param name="mediaName">Name of the Song to retrieve</param>
+        /// <returns>The Song reference</returns>
         public Song GetMedia(string mediaName)
         {
             return this.songs.Find(x=>x.Name == mediaName);
         }
-
+        /// <summary>
+        /// Removes a Song from the list
+        /// </summary>
+        /// <param name="mediaName">Song to remove</param>
         public void Remove(string mediaName)
         {
             Song s = this.songs.Find(x=>x.Name == mediaName);
             this.songs.Remove(s);
         }
-
+        /// <summary>
+        /// Removes a Song from the list
+        /// </summary>
+        /// <param name="index">Index of the Song to remove</param>
         public void RemoveAt(int index)
         {
             this.songs.RemoveAt(index);
         }
+        /// <summary>
+        /// Shuffles the list of Songs
+        /// </summary>
         public void shuffleSongs()
         {
             ListUtil.Shuffle(this.songs);
